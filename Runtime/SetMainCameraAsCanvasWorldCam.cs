9da8a3225c63a8c08fa29d6bed9087cb309168aa
mrstruijk <mrstruijk@gmail.com>
2024-11-22 18:00:55 +0100

    Updating Simplehelpers

---
+++ Runtime/SetMainCameraAsCanvasWorldCam.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+
+namespace SOSXR.SimpleHelpers
+{
+    [RequireComponent(typeof(Canvas))]
+    public class SetMainCameraAsCanvasWorldCam : MonoBehaviour
+    {
+        [SerializeField] [HideInInspector] private Canvas _canvas;
+
+
+        private void OnValidate()
+        {
+            if (_canvas != null)
+            {
+                return;
+            }
+
+            _canvas = GetComponent<Canvas>();
+            
+            FindWorldCamera();
+        }
+
+
+        private void Awake()
+        {
+            FindWorldCamera();
+        }
+
+
+        private void Update()
+        {
+            FindWorldCamera();
+        }
+
+
+        private void FindWorldCamera()
+        {
+            if (_canvas.worldCamera == null && Camera.main != null)
+            {
+                _canvas.worldCamera = Camera.main;
+
+                return;
+            }
+
+            enabled = false; // Disable this component as soon as we find a camera
+        }
+    }
+}
